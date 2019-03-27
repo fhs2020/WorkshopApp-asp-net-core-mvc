a53e99e71c28a078ee87ff552df2529c2c5d2f6b
Flavio H Sousa <beatyourcitation@outlook.com>
2019-03-27 06:56:01 -0300

    Foreign key not null (referential integrity)

--- SalesWebMVC/Services/SellerService.cs
+++ SalesWebMVC/Services/SellerService.cs
@@ -22,6 +22,8 @@ namespace SalesWebMVC.Services
 
         public void Insert(Seller obj)
         {
+            obj.Department = _context.Department.First();
+
             _context.Add(obj);
 
             _context.SaveChanges();
